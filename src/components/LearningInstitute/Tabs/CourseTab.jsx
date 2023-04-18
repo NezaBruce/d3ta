43ca2197354596d03ef4cf3f6533dd540d5fe255
nbrucepro <nezabruce@gmail.com>
2023-04-18 18:20:34 +0300

    [ft] - Todays's commits

--- src/components/LearningInstitute/Tabs/CourseTab.jsx
+++ src/components/LearningInstitute/Tabs/CourseTab.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import CoursesList from "../../Teachers/CoursesList";
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import CoursesList from "../../Teachers/CoursesList";
 
-function CourseTab({ instituteId, accordion }) {
-  const userPermissions = useSelector((state) => state.admin.userPermissions);
-  return (
-    <>
-      <CoursesList
-        instituteId={instituteId}
-        userPermissions={userPermissions}
-        type="institute"
-        accordion={accordion}
-      />
-    </>
-  );
-}
+// function CourseTab({ instituteId, accordion }) {
+//   const userPermissions = useSelector((state) => state.admin.userPermissions);
+//   return (
+//     <>
+//       <CoursesList
+//         instituteId={instituteId}
+//         userPermissions={userPermissions}
+//         type="institute"
+//         accordion={accordion}
+//       />
+//     </>
+//   );
+// }
 
-export default CourseTab;
+// export default CourseTab;
