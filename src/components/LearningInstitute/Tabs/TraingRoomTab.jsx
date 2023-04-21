0c19ca5af114f5fd7e0b53777224e8aff87f350b
nbrucepro <nezabruce@gmail.com>
2023-04-21 18:20:34 +0300

    [ft] - Todays's commits

--- src/components/LearningInstitute/Tabs/TraingRoomTab.jsx
+++ src/components/LearningInstitute/Tabs/TraingRoomTab.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import RoomListing from "../../PeerProducers/RoomListing";
+// import React from "react";
+// import RoomListing from "../../PeerProducers/RoomListing";
 
-function TraingRoomTab({ instituteId, accordion }) {
-  return (
-    <>
-      <RoomListing
-        instituteId={instituteId}
-        virtualEventType="training-room"
-        roleType="learning-institute"
-        accordion={accordion}
-      />
-    </>
-  );
-}
+// function TraingRoomTab({ instituteId, accordion }) {
+//   return (
+//     <>
+//       <RoomListing
+//         instituteId={instituteId}
+//         virtualEventType="training-room"
+//         roleType="learning-institute"
+//         accordion={accordion}
+//       />
+//     </>
+//   );
+// }
 
-export default TraingRoomTab;
+// export default TraingRoomTab;
