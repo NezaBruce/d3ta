499147b75679d52f3b4c7a30a71bd65e2085fdc8
nbrucepro <nezabruce@gmail.com>
2023-04-27 18:20:34 +0300

    [ft] - Todays's commits

--- src/components/LearningInstitute/CoursesPage.jsx
+++ src/components/LearningInstitute/CoursesPage.jsx
@@ -1,64 +1,64 @@
-import React, { useState } from "react";
-import { Tab, Tabs } from "react-bootstrap";
-import CourseTab from "./Tabs/CourseTab";
-import FacultyTab from "./Tabs/FacultyTab";
-import MasterClassTab from "./Tabs/MasterClassTab";
-import TraingRoomTab from "./Tabs/TraingRoomTab";
-import WebinarTab from "./Tabs/WebinarTab";
+// import React, { useState } from "react";
+// import { Tab, Tabs } from "react-bootstrap";
+// import CourseTab from "./Tabs/CourseTab";
+// import FacultyTab from "./Tabs/FacultyTab";
+// import MasterClassTab from "./Tabs/MasterClassTab";
+// import TraingRoomTab from "./Tabs/TraingRoomTab";
+// import WebinarTab from "./Tabs/WebinarTab";
 
-function CoursesPage({ instituteId, userId, accordion, type }) {
-  const [key, setKey] = useState("courses");
+// function CoursesPage({ instituteId, userId, accordion, type }) {
+//   const [key, setKey] = useState("courses");
 
-  return (
-    <Tabs
-      variant="pills"
-      id="uncontrolled-tab-example"
-      activeKey={key}
-      onSelect={(k) => setKey(k)}
-      className="mb-3 row pl-3"
-      mountOnEnter={`true`}
-      unmountOnExit={`true`}
-    >
-      <Tab
-        tabClassName="col-md-2 text-center"
-        eventKey="courses"
-        title="Courses"
-      >
-        <CourseTab instituteId={instituteId} accordion={accordion} />
-      </Tab>
-      {type === "University" && (
-        <Tab
-          tabClassName="col-md-2 text-center"
-          eventKey="faculty"
-          title="Faculties"
-        >
-          <FacultyTab instituteId={instituteId} />
-        </Tab>
-      )}
+//   return (
+//     <Tabs
+//       variant="pills"
+//       id="uncontrolled-tab-example"
+//       activeKey={key}
+//       onSelect={(k) => setKey(k)}
+//       className="mb-3 row pl-3"
+//       mountOnEnter={`true`}
+//       unmountOnExit={`true`}
+//     >
+//       <Tab
+//         tabClassName="col-md-2 text-center"
+//         eventKey="courses"
+//         title="Courses"
+//       >
+//         <CourseTab instituteId={instituteId} accordion={accordion} />
+//       </Tab>
+//       {type === "University" && (
+//         <Tab
+//           tabClassName="col-md-2 text-center"
+//           eventKey="faculty"
+//           title="Faculties"
+//         >
+//           <FacultyTab instituteId={instituteId} />
+//         </Tab>
+//       )}
 
-      <Tab
-        tabClassName="col-md-2 text-center"
-        eventKey="room"
-        title="Training Room"
-      >
-        <TraingRoomTab instituteId={instituteId} accordion={accordion} />
-      </Tab>
-      <Tab
-        tabClassName="col-md-2 text-center"
-        eventKey="webinar"
-        title="Webinar"
-      >
-        <WebinarTab instituteId={instituteId} accordion={accordion} />
-      </Tab>
-      <Tab
-        tabClassName="col-md-2 text-center"
-        eventKey="masterclass"
-        title="Master Class"
-      >
-        <MasterClassTab instituteId={instituteId} accordion={accordion} />
-      </Tab>
-    </Tabs>
-  );
-}
+//       <Tab
+//         tabClassName="col-md-2 text-center"
+//         eventKey="room"
+//         title="Training Room"
+//       >
+//         <TraingRoomTab instituteId={instituteId} accordion={accordion} />
+//       </Tab>
+//       <Tab
+//         tabClassName="col-md-2 text-center"
+//         eventKey="webinar"
+//         title="Webinar"
+//       >
+//         <WebinarTab instituteId={instituteId} accordion={accordion} />
+//       </Tab>
+//       <Tab
+//         tabClassName="col-md-2 text-center"
+//         eventKey="masterclass"
+//         title="Master Class"
+//       >
+//         <MasterClassTab instituteId={instituteId} accordion={accordion} />
+//       </Tab>
+//     </Tabs>
+//   );
+// }
 
-export default CoursesPage;
+// export default CoursesPage;
