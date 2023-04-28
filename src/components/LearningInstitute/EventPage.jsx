7312a5c0a3250a68f7434ee79eae391ca8c41cf6
nbrucepro <nezabruce@gmail.com>
2023-04-28 18:20:34 +0300

    [ft] - Todays's commits

--- src/components/LearningInstitute/EventPage.jsx
+++ src/components/LearningInstitute/EventPage.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { Card } from "react-bootstrap";
-import EventListing from "../PeerProducers/Event/EventListing";
-import PeopleCard from "./PeopleCard";
+// import React from "react";
+// import { Card } from "react-bootstrap";
+// import EventListing from "../PeerProducers/Event/EventListing";
+// import PeopleCard from "./PeopleCard";
 
-function EventPage({ instituteId, accordion }) {
-  return (
-    <>
-      <EventListing
-        instituteId={instituteId}
-        virtualEventType="event"
-        roleType="learning-institute"
-        accordion={accordion}
-      />
-    </>
-  );
-}
+// function EventPage({ instituteId, accordion }) {
+//   return (
+//     <>
+//       <EventListing
+//         instituteId={instituteId}
+//         virtualEventType="event"
+//         roleType="learning-institute"
+//         accordion={accordion}
+//       />
+//     </>
+//   );
+// }
 
-export default EventPage;
+// export default EventPage;
